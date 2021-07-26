e6fde6ceda2db21cc973302a5a8f5f24e10f7375
bakrtech <bakrtech@localhost.localdomain>
2021-07-26 10:32:55 +0530

    first one

---
+++ classp1.py
@@ -0,0 +1,14 @@
+print("helllo word")
+salary = int(input("Enter your salary here:"))
+DA =(125/100)*salary
+HRA =(10/100)*salary
+ALLOWNS = (15/100)*salary
+
+GROSS_INCOME = DA+HRA+ALLOWNS
+
+DEDCUTION = (12/100)*GROSS_INCOME
+
+NET_INCOME = GROSS_INCOME - DEDCUTION
+
+print("YOUR GROSS INCOME IS ",GROSS_INCOME,"AND NET INCOME IS ",NET_INCOME)
+print("THANK YOU FOR USING THE SALARY CALCULATOR!
