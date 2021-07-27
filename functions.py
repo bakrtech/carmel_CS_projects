bd61026a8e4bdd875ee5f3249e579457d1212223
abubakrniazi <abubakrniazi786@gmail.com>
2021-07-27 11:49:41 +0530

    ok

--- functions.py
+++ functions.py
@@ -3,3 +3,14 @@ Two types of functions:
 1 user defined
 2 in built functions
 """
+def formula_add():
+    a = int(input("Enter the value of a"))
+    b = int(input("Enter the value of b"))
+    c = a+b
+    return c
+sm =  formula_add()
+
+print("The sum of  a and b is ",sm)
+
+
+#
