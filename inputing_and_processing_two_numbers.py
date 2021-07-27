14bfb668324c362b2c9d6c2ea0f8432588f4f184
abubakrniazi <abubakrniazi786@gmail.com>
2021-07-27 12:25:22 +0530

    ok

---
+++ inputing_and_processing_two_numbers.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the fist value: "))
+b = int(input("Enter the second value: "))
+
+c =  a+b
+d = a-b
+e = a*b
+f = a/b
+print("The sum is ",c,", and substraction of the numbers is",d,", the multiplication of the numbers ",e," and the division is ",f)
