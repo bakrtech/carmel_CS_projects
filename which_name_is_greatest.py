f98ac0333150b17bfa9fb001d31966870a0bbbf2
abubakrniazi <abubakrniazi786@gmail.com>
2021-07-29 12:33:57 +0530

    ok

---
+++ which_name_is_greatest.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+if a > b:
+    if a > c:
+        print(a," Is the greatest ")
+if b > a:
+    if b > c:
+        print(b," is the greatest")
+if c > a:
+    if c > b:
+        print(c," is the g r eatest")
+if a ==b:
+    if b==c:
+        print("all are equal")
