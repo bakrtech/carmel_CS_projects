1cd1ac8c29586c856f5f3c71152ebbfcde57eeed
abubakrniazi <abubakrniazi786@gmail.com>
2021-07-29 12:02:32 +0530

    ok

---
+++ genrate_oddnumbers_between_50_to_90.py
@@ -0,0 +1,18 @@
+#odd numbers
+for i in range(50, 90):
+    if i % 2 != 0:
+        # print("")
+        # to have it in a straight line
+        print(i, end=" ")
+
+
+
+print(" ")
+
+# even numbers
+for i in range(50, 90):
+    if i % 2 == 0:
+        # print(i)
+        # to have it in a straight line
+        print(i)
+
