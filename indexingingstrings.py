e1be990852234fe40f6ec26c815abff6a3406256
abubakrniazi <abubakrniazi786@gmail.com>
2021-08-10 11:14:20 +0530

    SCHOOL PYTHON XD

---
+++ indexingingstrings.py
@@ -0,0 +1,8 @@
+a = "HELLO WORLD"
+# here every character has a number like H is 0 and D is 11
+
+
+print(a[0]) # output must be H
+print("writing using for loops")
+for i in range(len(a)):
+    print(a[i])
