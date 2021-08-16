a19f1c21846f62bd068ebb42d7d7ee861a8c51fe
abubakrniazi <abubakrniazi786@gmail.com>
2021-08-16 11:41:46 +0530

    SCHOOL PYTHON XD

--- dict_tut.py
+++ dict_tut.py
@@ -1,8 +1,8 @@
-dict1 ={'ant':1,'bat':1,'cat':3,True:4}
+dict1 = {'ant': 1, 'bat': 1, 'cat': 3, True: 4}
 print(dict1)
 print(dict1.values())
 print(dict1.keys())
-# IS UNODERED
-# KEY IS IMUTABLE AND VALUE IS MUTALBLE
+# IS UNORDERED
+# KEY IS IMMUTABLE AND VALUE IS MUTABLE
 print(dict1['ant'])
 print(id(dict1))
