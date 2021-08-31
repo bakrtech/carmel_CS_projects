5da3233da09f9e75d321f55a9623009b226cb7f8
root <abubakrniazi786@gmail.com>
2021-08-31 12:01:11 +0530

    SCHOOL PYTHON XD

---
+++ fabonacci series.py
@@ -0,0 +1,11 @@
+t =int(input("how many terms? (enter 2+ value only): "))
+f =0
+s = 1
+print("/n fibonacci series is: ")
+print(f,",",s,end=",")
+for i in range(2,t):
+    third = f +s
+    f =s
+    s = third
+    print(s,end="")
+
